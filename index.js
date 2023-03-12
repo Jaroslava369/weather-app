409a9d39aaff1b839b93efea4406faa7cdf87912
Jaroslava369 <125553051+Jaroslava369@users.noreply.github.com>
2023-03-12 21:51:20 +0100

    Added Celsius/Fahrenheit conversion

--- index.js
+++ index.js
@@ -41,8 +41,20 @@ function showTemperature(response) {
   console.log(response);
   let cityName = document.querySelector("#maincitySearch");
   cityName.innerHTML = response.data.name;
+  celsiusTemperature = response.data.main.temp;
   let mainTemperature = document.querySelector("#temperature");
-  mainTemperature.innerHTML = Math.round(response.data.main.temp);
+  mainTemperature.innerHTML = Math.round(celsiusTemperature);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function search(event) {
@@ -61,25 +73,33 @@ function search(event) {
   axios.get(apiUrl).then(showTemperature);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let mainTemperature = document.querySelector("#temperature");
+
+  celsiusTemp.classList.remove("active");
+  fahrenheitTemp.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  mainTemperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusTemp.classList.add("active");
+  fahrenheitTemp.classList.remove("active");
+  let mainTemperature = document.querySelector("#temperature");
+  mainTemperature.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 let form = document.querySelector(".searchcity");
 form.addEventListener("submit", search);
 
-function showCurrent() {
-  function getPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiUrl2 =
-      "https://api.openweathermap.org/data/2.5/weather?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&appid=" +
-      apiKey +
-      "&units=metric";
-    axios.get(apiUrl2).then(showTemperature);
-  }
+let fahrenheitTemp = document.querySelector("#fahrenheitTemp");
+fahrenheitTemp.addEventListener("click", displayFahrenheitTemperature);
 
-  navigator.geolocation.getCurrentPosition(getPosition);
-}
-let current = document.querySelector("#current");
-current.addEventListener("click", showCurrent);
+let celsiusTemp = document.querySelector("#celsiusTemp");
+celsiusTemp.addEventListener("click", displayCelsiusTemperature);
+
+search("Poprad");
